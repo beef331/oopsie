7372c705f9907cafe01aba9732862b777a9f88a5
Jason <beefers331@gmail.com>
2021-06-20 15:20:55 -0600

    Added tests and casestatment

--- src/oopsie.nim
+++ src/oopsie.nim
@@ -8,7 +8,19 @@ proc superImpl(obj: NimNode): NimNode =
   assert impl[1].kind == nnkOfInherit
   impl[1][0]
 
-macro super*(obj: ref object): untyped =
+macro inherits*(a: typed): bool =
+  var impl = a.getImpl
+  result = newLit false
+  if impl.kind == nnkNilLit:
+    impl = a.getTypeImpl[0].getImpl
+    result = newLit impl.kind == nnkTypeDef and impl[2].kind == nnkObjectTy and impl[2][1].kind == nnkOfInherit
+  else:
+    result = newLit impl.kind == nnkTypeDef and impl[2].kind == nnkRefTy and impl[2][0][1].kind == nnkOfInherit
+
+type Inherits = concept c
+  c.inherits
+
+macro super*(obj: Inherits): untyped =
   ## Gets the parent of this `obj`
   runnableExamples:
     type 
@@ -19,7 +31,7 @@ macro super*(obj: ref object): untyped =
 
   obj.superImpl
 
-macro rootSuper*(obj: ref object): untyped =
+macro rootSuper*(obj: Inherits): untyped =
   ## Gets the root parent of this `obj`, 
   ## does not get `RootObj`
   runnableExamples:
@@ -48,7 +60,7 @@ func getFields(child: NimNode): seq[NimNode] =
   if not impl[^1][1][0].eqIdent("RootObj"):
     result.add getFields(impl[^1][1][0])
 
-macro copy*(child: ref object, parent: ref object): untyped =
+macro copy*[C: Inherits, P: Inherits](child: C, parent: P): untyped =
   ## Allows copying from a parent type to a child
   runnableExamples:
     type 
@@ -72,7 +84,7 @@ macro copy*(child: ref object, parent: ref object): untyped =
     result.add quote do:
       `child`.`field` = `parent`.`field`
 
-macro copyAs*(parent: ref object, child: typedesc, name: untyped): untyped =
+macro copyAs*(parent: Inherits, child: typedesc, name: untyped): untyped =
   ## Duplicates an object into a new object of `child` with `name`
   runnableExamples:
     type 
@@ -89,3 +101,52 @@ macro copyAs*(parent: ref object, child: typedesc, name: untyped): untyped =
   for x in fields:
     result.add nnkExprColonExpr.newTree(x, newDotExpr(parent, x))
   result = newVarStmt(name, result)
+
+proc caseImpl(stmt: NimNode): NimNode =
+  let obj = stmt[0]
+  result = nnkIfStmt.newTree
+  for branch in stmt[1..^1]:
+    if branch.kind != nnkElse:
+      for cond in branch[0..^2]:
+        let
+          body = branch[^1].copyNimTree
+          it = ident"it"
+        body.insert 0, quote do:
+          let `it` = `cond`(`obj`)
+        result.add nnkElifBranch.newTree(nnkInfix.newTree(ident"of", obj, cond), body)
+    else:
+      result.add branch
+
+when (NimMajor, NimMinor) < (1, 5):
+  macro match*(obj: Inherits): untyped =
+    ## A more ergonomic abstraction for nested elif branches.
+    ## Injects `it` as the converted type internally.
+    ## Else branch does not inject an `it`.
+    runnableExamples:
+      {.experimental: "caseStmtMacros".}
+      type
+        A = ref object of RootObj
+        B = ref object of A
+          a: int
+      case A(B()): # This is an `A` held `B`
+      of B:
+        assert $(it[]) == "(a: 0)"
+      else: discard
+
+    caseImpl(obj)
+else:
+  macro `case`*(obj: Inherits): untyped =
+    ## A more ergonomic abstraction for nested elif branches.
+    ## Injects `it` as the converted type internally.
+    ## Else branch does not inject an `it`.
+    runnableExamples:
+      {.experimental: "caseStmtMacros".}
+      type
+        A = ref object of RootObj
+        B = ref object of A
+          a: int
+      case A(B()): # This is an `A` held `B`
+      of B:
+        assert $(it[]) == "(a: 0)"
+      else: discard
+    caseImpl(obj)
--- tests/test1.nim
+++ tests/test1.nim
@@ -1,12 +1,53 @@
-# This is just an example to get you started. You may wish to put all of your
-# tests into a single file, or separate them into multiple `test1`, `test2`
-# etc. files (better names are recommended, just make sure the name starts with
-# the letter 't').
-#
-# To run these tests, simply execute `nimble test`.
-
 import unittest
-
 import oopsie
-test "can add":
-  check add(5, 5) == 10
+
+test "Super":
+  type 
+    A = ref object of RootObj
+    B = ref object of A
+  assert B().super is A
+  assert A().super is RootObj
+
+test "Root Super":
+  type 
+    A = ref object of RootObj
+    B = ref object of A
+    C = ref object of B
+  assert C().rootSuper is A
+  assert B().rootSuper is A
+  assert A().rootSuper is A
+
+
+test "Copy":
+  type 
+    A = ref object of RootObj
+      a, b, c: int
+    B = ref object of A
+      d: float
+  var
+    a = A(a: 100, b: 200, c: 20)
+    b = B()
+  b.copy(a)
+  assert b[] == B(a: 100, b: 200, c: 20)[]
+
+
+
+test "Copy As":
+  type 
+    A = ref object of RootObj
+      a, b, c: int
+    B = ref object of A
+      d: float
+  var a = A(a: 100, b: 200, c: 20)
+  a.copyAs(B, newVar)
+  assert newVar[] == B(a: 100, b: 200, c: 20)[]
+
+test "Case Stmt":
+  type
+    A = ref object of RootObj
+    B = ref object of A
+      a: int
+  case A(B()):
+  of B:
+    assert $(it[]) == "(a: 0)"
+  else: discard
